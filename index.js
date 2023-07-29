d649fd0efdb5f07463350e5bbeb2209924618716
Adrienne Kecskes <akecs619@yahoo.com>
2023-07-28 19:52:27 -0500

    make logo and text work

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import inquirer from "inquirer";
+import fs from "fs";
 inquirer
   .prompt([
     {
@@ -25,12 +26,35 @@ inquirer
     }
   
   ])
-  .then((answers) => {
-    const svg = new svg(
-      answers.brandInitials,
-      answers.textColor,
-      answers.shape,
-      answers.shapeColor
-    );
-  });
+  .then(({brandInitials, textColor, shape, shapeColor}) => {
+    let SVGlogo;
+    switch(shape){
+      case "Circle":
+        SVGlogo = `<circle cx="150" cy="100" r="80" fill="${shapeColor}" />`;
+        
+        break;
+
+      // case "square":
+      //     shape = new Square();
+      //     break;
+
+      // case "triangle":
+      //     shape = new Triangle();
+      //     break;
+
+       default:
+            break;
+    }
+// create a variable to hold the SVG text string with the proper ${brandInitials} and ${textColor} 
+// create the SVG container with the ${shape} ${SVGtextString}
+// write file with fs
+if (brandInitials.length > 3) {
+  throw new Error("Text must not exceed 3 characters.");
+}
+const SVGtextString = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${brandInitials}</text>`;
+const SVGContainer = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${SVGlogo}${SVGtextString}</svg>`;
+fs.writeFile("logo.svg",SVGContainer, (error) => {if (error) console.log(error)})
+})
+
+  
 
--- shapes.test.js
+++ shapes.test.js
@@ -1,17 +1,56 @@
-test('generateLogoSVG should generate an SVG logo witha square shape', () => {
-    const size = 200;
-    const text ='square logo';
-    const textColor = '#000FF';
-    const shape = 'square';
-    const shapeColor = '#FF0000';
+const { Square, Triangle, Circle } = require("./shapes");
 
-const expectedSVG =
-<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 400 400">
-<rect width="100%" height="100%" fill="#FFF00"/>
-<text x="50%" y="50%" dominant-baseline= "middle" text-anchor="middle" style="text-align: center; fill="${textColor}">${text}</text>
-</svg>
-;
+describe("Circle", () => {
+  test("should render svg for a green circle element", () => {
+    const expectedSvg = '<circle cx="150" cy="100" r="80" fill="green" />';
+    const circle = new Circle();
+    circle.setColor("green");
+    const actualSvg = circle.render();
+    expect(actualSvg).toEqual(expectedSvg);
+  });
+  test("should accept a fillColor param", () => {
+    const expectedSvg = '<circle cx="150" cy="100" r="80" fill="blue" />';
+    const circle = new Circle();
+    circle.setColor("blue");
+    const actualSvg = circle.render();
+    expect(actualSvg).toEqual(expectedSvg);
+  });
+});
 
-expect(generateLogoSVG(size, text, textColor, shape, shapeColor)).toEqual(expectedSVG);
+describe("Triangle", () => {
+  test("should render svg for a green polygon element", () => {
+    const expectedSvg =
+      '<polygon points="150, 18 244, 182 56, 182" fill="bisque" />';
+    const triangle = new Triangle();
+    triangle.setColor("bisque");
+    const actualSvg = triangle.render();
+    expect(actualSvg).toEqual(expectedSvg);
+  });
+  test("should accept a fillColor param", () => {
+    const expectedSvg =
+      '<polygon points="150, 18 244, 182 56, 182" fill="purple" />';
+    const triangle = new Triangle();
+    triangle.setColor("purple");
+    const actualSvg = triangle.render();
+    expect(actualSvg).toEqual(expectedSvg);
+  });
+});
 
-})
+describe("Square", () => {
+  test("should render svg for a green polygon element", () => {
+    const expectedSvg =
+      '<rect x="90" y="40" width="120" height="120" fill="dodgerblue" />';
+    const square = new Square();
+    square.setColor("dodgerblue");
+    const actualSvg = square.render();
+    expect(actualSvg).toEqual(expectedSvg);
+  });
+  test("should accept a fillColor param", () => {
+    const expectedSvg =
+      '<rect x="90" y="40" width="120" height="120" fill="red" />';
+    const square = new Square();
+    square.setColor("red");
+    const actualSvg = square.render();
+    expect(actualSvg).toEqual(expectedSvg);
+  });
+});
